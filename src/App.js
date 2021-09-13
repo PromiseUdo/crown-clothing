35fe32f1731eb24b6fe8915c6948dca3add62db4
Promise Udo <info.promiseudo@gmail.com>
2021-09-13 08:48:20 +0100

    Implemented firebase utils, included ability to store authenticated users into firestore database

--- src/App.js
+++ src/App.js
@@ -5,23 +5,35 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component{
   constructor(){
     super();
     this.state = {
       currentUser: null
-    }
+    };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
-      console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef =  await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {  
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+      }else{
+        this.setState({currentUser: userAuth})
+      }
+    });
   }
 
   componentWillUnmount(){
@@ -32,6 +44,7 @@ class App extends React.Component{
     return(
       <div>
         <Header currentUser = {this.state.currentUser}/>
+        {console.log(this.state.currentUser, "from app.js")}
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
